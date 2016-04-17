12078961c9d859a1d57cff6ec17b81f69cd92647
Will Farrington <wcfarrington@gmail.com>
2016-04-17 13:30:18 -0400

    Initial commit

---
+++ config.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+)
+
+// Config contains a set of buttons
+type Config struct {
+	Buttons []Button
+}
+
+// Button holds some configuration for a set of Dash Buttons
+type Button struct {
+	Name string
+	Address string
+	Interface string
+	URL string
+	Method string
+}
+
+// LoadConfigFromFile reads a JSON file at path and returns
+// a fully populated configuration from it and an error
+func LoadConfigFromFile(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	
+	raw, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	
+	var config Config
+	err = json.Unmarshal(raw, &config)
+	return &config, err
+}
---
+++ main.go
@@ -0,0 +1,153 @@
+// Much of this code was directly inspired by https://github.com/mikeflynn/go-dash-button
+// Lacking a license on that project, I can't really think of anything better than a
+// notice about this in the source code.
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"sync"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	configPath = flag.String("config", "", "The configuration file")
+)
+
+func main() {
+	flag.Parse()
+	if *configPath == "" {
+		log.Fatalln("expected a config, but none was given")
+	}
+	
+	config, err := LoadConfigFromFile(*configPath)
+	boomtown(err)
+	
+	ifaces, err := net.Interfaces()
+	boomtown(err)
+
+	log.Println("got interfaces")
+
+	var wg sync.WaitGroup
+
+	for _, iface := range ifaces {
+		wg.Add(1)
+
+		go func(iface net.Interface) {
+			defer wg.Done()
+			if err := scan(&iface, config); err != nil {
+				log.Printf("interface %v: %v\n", iface.Name, err)
+			}
+		}(iface)
+	}
+
+	wg.Wait()
+}
+
+func scan(iface *net.Interface, config *Config) error {
+	var addr *net.IPNet
+
+	addrs, err := iface.Addrs()
+	boomtown(err)
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok {
+			if ip4 := ipnet.IP.To4(); ip4 != nil {
+				addr = &net.IPNet{IP: ip4, Mask: ipnet.Mask[len(ipnet.Mask)-4:]}
+				break
+			}
+		}
+	}
+
+	if addr == nil {
+		return errors.New("no good IP network found")
+	}
+	if addr.IP[0] == 127 {
+		return errors.New("skipping localhost")
+	}
+	log.Printf("Using network range %v for interface %v\n", addr, iface.Name)
+
+	handle, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+	boomtown(err)
+	defer handle.Close()
+
+	stop := make(chan struct{})
+	readARP(handle, iface, stop, config)
+	defer close(stop)
+
+	return err
+}
+
+func readARP(handle *pcap.Handle, iface *net.Interface, stop chan struct{}, config *Config) {
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	in := src.Packets()
+	for {
+		var packet gopacket.Packet
+		select {
+		case <-stop:
+			return
+		case packet = <-in:
+			arpLayer := packet.Layer(layers.LayerTypeARP)
+			if arpLayer == nil {
+				continue
+			}
+
+			arp := arpLayer.(*layers.ARP)
+
+			if !net.IP(arp.SourceProtAddress).Equal(net.ParseIP("0.0.0.0")) {
+				continue
+			}
+			
+			found := false
+			for _, btn := range config.Buttons {
+				if net.HardwareAddr(arp.SourceHwAddress).String() == btn.Address {
+					found = true
+					log.Printf("received ping for button '%v'", btn.Name)
+					
+					if btn.URL != "" && btn.Method != "" {
+						go dispatchHTTPRequest(btn.Method, btn.URL)
+					}
+				}
+			}
+			
+			if !found {
+				log.Printf("received ping for unknown MAC addr: %v\n", net.HardwareAddr(arp.SourceHwAddress))
+			}
+		}
+	}
+}
+
+func dispatchHTTPRequest(method, uri string) {
+	parsed, err := url.Parse(uri)
+	if err != nil {
+		log.Fatalln("invalid url")
+	}
+	
+	if parsed.Scheme == "" {
+		parsed.Scheme = "http"
+	}
+	
+	switch method {
+	case "GET":
+		resp, err := http.Get(parsed.String())
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Printf("GET %v returned status %q\n", parsed, resp.Status)
+		}
+	}
+}
+
+func boomtown(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
